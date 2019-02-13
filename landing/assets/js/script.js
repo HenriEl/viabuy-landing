b4d9a261e9d489d459a30f4eda17897f06a0dcc6
henriel <henrielezi1@gmail.com>
2019-02-13 17:40:55 +0100

    Added video support

--- landing/assets/js/script.js
+++ landing/assets/js/script.js
@@ -1,9 +1,28 @@
 var modal = document.querySelector(".modal");
-var openModal = document.querySelector(".open-modal");
+var openModal = document.getElementById("open-video");
+var openModalFaceID = document.getElementById("open-video-face-id");
+var openModalTouchID = document.getElementById("open-video-touch-id");
 var closeModal = document.querySelector(".close-modal");
+var video = document.getElementById("video-popup");
 
-function toggleModal() {
+function toggleModal(selectedVideo) {
+
+    if(selectedVideo == "open-video") {
+        video.src = "assets/videos/AddCard_DE-Slide.mp4";
+    } else if(selectedVideo == "open-video-face-id") {
+        video.src = "assets/videos/04_FACEID_DE_v01.mp4";
+    } else if(selectedVideo == "open-video-touch-id") {
+        video.src = "assets/videos/build_03_TOUCHID_v041.mp4";
+    }
+
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+
     modal.classList.toggle("show-modal");
+    closeModal.classList.toggle("show-modal");
 }
 
 function windowOnClick(event) {
@@ -12,7 +31,9 @@ function windowOnClick(event) {
     }
 }
 
-openModal.addEventListener("click", toggleModal);
+openModal.addEventListener("click", function(){toggleModal(openModal.id)});
+openModalFaceID.addEventListener("click", function(){toggleModal(openModalFaceID.id)});
+openModalTouchID.addEventListener("click", function(){toggleModal(openModalTouchID.id)});
 closeModal.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
